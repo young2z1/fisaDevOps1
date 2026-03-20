da1f9291f3f7a33214b24ca8cd251a252c05cf37
young2z1 <fftizlk0@gmail.com>
2026-03-20 15:56:31 +0900

    Initial commit for Spring Boot Gradle project

---
+++ src/main/java/edu/fisa/ce/Step06BuildGradleTestApplication.java
@@ -0,0 +1,13 @@
+package edu.fisa.ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step06BuildGradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step06BuildGradleTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fisa/ce/Step06BuildGradleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fisa.ce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step06BuildGradleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
